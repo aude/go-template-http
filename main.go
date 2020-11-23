5cc556a0bdc530ec1b680b6bcaf800034e3c1808
Asbjørn Apeland <aude@users.noreply.github.com>
2020-11-23 12:28:42 +0100

    first CLI version

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+func usage() string {
+	return fmt.Sprintf("usage: %s <template>", os.Args[0])
+}
+
 func main() {
-	println("Hello, world!")
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, usage())
+		os.Exit(1)
+	}
+	in := os.Args[1]
+
+	t := template.New("template")
+
+	t, err := t.Parse(in)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "ERROR: parse template:", err)
+		os.Exit(1)
+	}
+
+	if err := t.Execute(os.Stdout, nil); err != nil {
+		fmt.Fprintln(os.Stderr, "ERROR: execute template:", err)
+		os.Exit(1)
+	}
 }
